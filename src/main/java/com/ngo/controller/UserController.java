5a27dc4d45f78c19ebe442fe92acf7468607067f
nanduys <nanduys@gmail.com>
2019-12-22 08:16:16 -0800

    hgjhgj

--- src/main/java/com/ngo/controller/UserController.java
+++ src/main/java/com/ngo/controller/UserController.java
@@ -1,7 +1,5 @@
 package com.ngo.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,7 +7,6 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ngo.UserForm;
@@ -31,20 +28,6 @@ public class UserController {
 		return ResponseEntity.status(HttpStatus.OK).body(savedUserForm);
 	}
 	
-	@CrossOrigin(origins = "http://localhost:4200")
-	@RequestMapping(value = "/getAllUsers", method = RequestMethod.GET)
-	public ResponseEntity<List<UserForm>> getAllUsers() {
 
-		List<UserForm> userFormList = userService.getAllUsers();
-		return ResponseEntity.status(HttpStatus.OK).body(userFormList);
-	}
-	
-	@CrossOrigin(origins = "http://localhost:4200")
-	@RequestMapping(value = "/deleteUser", method = RequestMethod.GET)
-	public boolean delete(@RequestParam(name = "id") Long id) {
-	boolean isdeleted =	userService.delete(id);
-		return isdeleted;
-	}
-	
 
 }
--- src/main/java/com/ngo/service/UserService.java
+++ src/main/java/com/ngo/service/UserService.java
@@ -1,8 +1,5 @@
 package com.ngo.service;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -29,29 +26,7 @@ public class UserService {
 		return userForm;
 	}
 	
-	public List<UserForm> getAllUsers() {
 
-		Iterable<User> userList = userRepository.findAll();
-		List<UserForm> userformList = new ArrayList<UserForm>();
-
-		for (User temp : userList) {
-			UserForm userForm = new UserForm();
-			BeanUtils.copyProperties(temp, userForm);
-			userformList.add(userForm);
-		}
-		return userformList;
-	}
-	
-	public boolean delete(Long id) {
-		try {
-			userRepository.delete(id);
-		} catch (Exception e) {
-			e.printStackTrace();
-			return false;
-		}
-		return true;
-	}
-	
 
 
 }
